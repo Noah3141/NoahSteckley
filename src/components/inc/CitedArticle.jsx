781ddeb5904759c9708ed6bf0b3778feac4c7bd3
Noah S <Noah3141@gmail.com>
2023-04-11 21:00:41 -0400

    Integrated quotes to fix deploy now

--- src/components/inc/CitedArticle.jsx
+++ src/components/inc/CitedArticle.jsx
@@ -17,11 +17,6 @@ function CitedArticle(props) {
     quote_3,
   } = props;
 
-  type = type;
-  quote_1 = quote_1;
-  quote_2 = quote_2;
-  quote_3 = quote_3;
-
   return (
     <div className="card mb-5 border-dark bg-dark border-3 shadow">
       <h5 className="card-header bg-dark text-danger">{title}</h5>
@@ -31,8 +26,14 @@ function CitedArticle(props) {
           <p>{results}</p>
           <p>{concl}</p>
           <p>{restrs}</p>
+          <ul>
+            {quote_1 && <li>{quote_1}</li>}
+            {quote_2 && <li>{quote_2}</li>}
+            {quote_3 && <li>{quote_3}</li>}
+          </ul>
         </div>
       </div>
+      <span type="hidden">{type}</span>
       <div className="card-footer bg-info-subtle text-info  ">
         {authors} ({year}). {title}. <i>{journal}</i>
         {pub_pp}.
